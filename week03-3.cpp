1470aea62dc1ca4861d9de5768a0aa2e6d7d6520
csy1141 <79676994+csy1141@users.noreply.github.com>
2021-03-12 10:54:26 +0800

    Add files via upload

---
+++ week03-3.cpp
@@ -0,0 +1,29 @@
+#include <stdio.h>
+int a[5]={0, 10, 20, 30, 40};
+void printfAll(){
+    for(int i=0; i<=4; i++){
+        printf("%d ", a[i]);
+    }
+    printf("\n");
+}
+int main()
+{
+    printfAll();
+
+    int *p= &a[2];
+    *p=222;
+    printfAll();
+    printf("¶Ã½X:%d\n", p);
+
+    p=p+2;
+    *p=666;
+    printfAll();
+    printf("¶Ã½X:%d\n", p);
+
+    p--;
+    *p=555;
+    printfAll();
+    printf("¶Ã½X:%d\n", p);
+
+
+}
